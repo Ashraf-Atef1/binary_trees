e386a630987f29635504fffe0fe00bf8f5164d79
Ashraf Atef <ashraf.programer@gmail.com>
2024-03-26 08:38:27 +0200

    task

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -41,16 +41,12 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left, right;
+	size_t l = 0;
+	size_t r = 0;
 
-	if (!tree)
-		return (-1);
-	if (!tree->left && !tree->right)
+	if (tree == NULL)
 		return (0);
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
-	if (left > right)
-		return (left + 1);
-	else
-		return (right + 1);
+	l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+	r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+	return ((l > r) ? l : r);
 }
