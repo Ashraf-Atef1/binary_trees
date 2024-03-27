9816e89186923f721ced650ca2d32cdc0dadb22f
Ashraf Atef <ashraf.programer@gmail.com>
2024-03-27 23:18:53 +0200

    task

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,7 +1,51 @@
 #include "binary_trees.h"
 
-size_t binary_tree_size(const binary_tree_t *tree);
-binary_tree_t *check_balance(binary_tree_t *node, binary_tree_t *root);
+/**
+ * fix_balance - Fixes the balance of an AVL tree after insertion
+ * @tree: Pointer to a pointer to the root node of the AVL tree
+ * @value: Value of the newly inserted node
+ *
+ * This function fixes the balance of the AVL tree rooted at the specified node
+ * after inserting a new node with the given value.
+ * It checks the balance factor of the tree and performs rotations
+ * if necessary to maintain the AVL property.
+ *
+ * @tree: Pointer to a pointer to the root node of the AVL tree
+ * @value: Value of the newly inserted node
+ */
+void fix_blance(avl_t **tree, int value)
+{
+	int bfactor = binary_tree_balance(*tree);
+
+	if (bfactor > 1 && (*tree)->left->n > value)
+		*tree = binary_tree_rotate_right(*tree);
+	else if (bfactor < -1 && (*tree)->right->n < value)
+		*tree = binary_tree_rotate_left(*tree);
+	else if (bfactor > 1 && (*tree)->left->n < value)
+	{
+		(*tree)->left = binary_tree_rotate_left((*tree)->left);
+		*tree = binary_tree_rotate_right(*tree);
+	}
+	else if (bfactor < -1 && (*tree)->right->n > value)
+	{
+		(*tree)->right = binary_tree_rotate_right((*tree)->right);
+		*tree = binary_tree_rotate_left(*tree);
+	}
+}
+/**
+ * avl_insert - Inserts a value into an AVL tree
+ * @tree: Pointer to a pointer to the root node of the AVL tree
+ * @value: Value to insert into the AVL tree
+ *
+ * This function inserts a new node with the given value into the AVL tree
+ * pointed to by the specified pointer to the root node. After insertion,
+ * it fixes the balance of the tree using the fix_balance() function.
+ *
+ * @tree: Pointer to a pointer to the root node of the AVL tree
+ * @value: Value to insert into the AVL tree
+ *
+ * Return: Pointer to the newly inserted node, or NULL if the insertion fails
+ */
 avl_t *avl_insert(avl_t **tree, int value)
 {
 	bst_t *new_node = binary_tree_node(NULL, value), *current_node = *tree;
@@ -29,109 +73,8 @@ avl_t *avl_insert(avl_t **tree, int value)
 	}
 	else
 		*tree = new_node;
-
-	check_balance(new_node, *tree);
+	while (current_node)
+		fix_blance(current_node == *tree ? tree : &current_node, value),
+			current_node = current_node->parent;
 	return (new_node);
-}
-/**
- * binary_tree_size - Measures the size of a binary tree
- * @tree: Pointer to the root node of the tree
- *
- * This function measures the size of the binary tree
- * rooted at the specified node.
- * The size of a binary tree is defined as
- * the total number of nodes in the tree.
- * The size of an empty tree is 0.
- *
- * @tree: Pointer to the root node of the tree
- *
- * Return: Size of the binary tree, or 0 if the tree is NULL
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
-}
-/**
- * node_is_complete - Checks if a node has both left and right children
- * @node: Pointer to the node to be checked
- *
- * This function checks whether the specified node has both
- * left and right children.
- * If both children are present, the function returns 1;
- * otherwise, it returns 0.
- *
- * @node: Pointer to the node to be checked
- *
- * Return: 1 if the node has both left and right children, 0 otherwise or if
- *         the node is NULL
- */
-int node_is_complete(const binary_tree_t *node)
-{
-	if (node && node->left && node->right)
-		return (1);
-	return (0);
-}
-int check_balance_helper(const binary_tree_t *node)
-{
-	if (!node || !node->parent ||
-		!node->parent->parent ||
-		!node->parent->parent->parent ||
-		node_is_complete(node->parent) ||
-		node_is_complete(node->parent->parent))
-		return (1);
-	return (0);
-}
-void swap_nodes(binary_tree_t *first, binary_tree_t *second)
-{
-	binary_tree_t *tmp;
-
-	if (!first || !second)
-		return;
-	second->parent = first->parent;
-	first->parent = second;
-	if (second->parent && second->parent->left == first)
-		second->parent->left = second;
-	if (second->parent && second->parent->right == first)
-		second->parent->right = second;
-	if (first->left == second)
-		first->left = second->right,
-		second->right = first,
-		tmp = first->right,
-		first->right = second->left,
-		second->left = tmp;
-	if (first->right == second)
-		first->right = second->left,
-		second->left = first,
-		tmp = first->left,
-		first->left = second->right,
-		second->right = tmp;
-	if (first->left)
-		first->left->parent = first;
-	if (first->right)
-		first->right->parent = first;
-	if (second->left)
-		second->left->parent = second;
-	if (second->right)
-		second->right->parent = second;
-}
-binary_tree_t *check_balance(binary_tree_t *node, binary_tree_t *root)
-{
-	binary_tree_t *parent, *grand_parent;
-
-	if (check_balance_helper(node))
-		return (root);
-	parent = node->parent, grand_parent = node->parent->parent;
-	if (parent->left && parent->right)
-		return (root);
-	if (parent->left && grand_parent->left == parent)
-		grand_parent->parent = binary_tree_rotate_right(grand_parent);
-	else if (parent->right && grand_parent->right == parent)
-		grand_parent->parent = binary_tree_rotate_left(grand_parent);
-	else if (parent->left && grand_parent->right == parent)
-		swap_nodes(parent, node), root = binary_tree_rotate_left(grand_parent);
-	else if (parent->right && grand_parent->left == parent)
-		swap_nodes(parent, node), root = binary_tree_rotate_right(grand_parent);
-	return (root);
 }
--- 121-main.c
+++ 121-main.c
@@ -11,31 +11,23 @@ int main(void)
 {
 	avl_t *root;
 	avl_t *node;
+	int array[] = {
+		98, 110, 43, 56, 2, 78, 123, 76, 234};
+	size_t size = sizeof(array) / sizeof(array[0]);
+	size_t i;
 
 	root = NULL;
-	node = avl_insert(&root, 98);
-	printf("Inserted: %d\n", node->n);
-	binary_tree_print(root);
-	node = avl_insert(&root, 402);
-	printf("\nInserted: %d\n", node->n);
-	binary_tree_print(root);
-	node = avl_insert(&root, 12);
-	printf("\nInserted: %d\n", node->n);
-	binary_tree_print(root);
-	node = avl_insert(&root, 46);
-	printf("\nInserted: %d\n", node->n);
-	binary_tree_print(root);
-	node = avl_insert(&root, 128);
-	printf("\nInserted: %d\n", node->n);
-	binary_tree_print(root);
-	node = avl_insert(&root, 256);
-	printf("\nInserted: %d\n", node->n);
-	binary_tree_print(root);
-	node = avl_insert(&root, 512);
-	printf("\nInserted: %d\n", node->n);
-	binary_tree_print(root);
-	node = avl_insert(&root, 50);
-	printf("\nInserted: %d\n", node->n);
+	for (i = 0; i < size; i++)
+	{
+		node = avl_insert(&root, array[i]);
+		if (node)
+			printf("Value inserted: %d\n", node->n);
+		else
+		{
+			printf("Value not inserted: %d\n", array[i]);
+		}
+	}
+
 	binary_tree_print(root);
 	return (0);
 }
