6d5e278f212cb828a7aeb2bd0d0bf5440afc298b
Ashraf Atef <ashraf.programer@gmail.com>
2024-03-28 07:24:40 +0200

    task

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -45,26 +45,3 @@ int heap_extract(heap_t **root)
 			break;
 	return (i);
 }
-
-/**
- * binary_tree_node - Creates a new binary tree node
- * @parent: Pointer to the parent node of the new node
- * @value: Value to be stored in the new node
- *
- * This function creates a new binary tree node with the given value
- * and sets its parent pointer to the provided parent node.
- *
- * Return: Pointer to the newly created node, or NULL on failure
- */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
-	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
-
-	if (!newNode)
-		return (NULL);
-	newNode->n = value;
-	newNode->left = NULL;
-	newNode->right = NULL;
-	newNode->parent = parent;
-	return (newNode);
-}
